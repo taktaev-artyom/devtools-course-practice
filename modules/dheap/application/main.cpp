66d9d48dfb453bf8a5419132836be479e25c8f77
taktaev-artyom <artaktaev@gmail.com>
2021-05-17 00:32:23 +0300

    deleted namespace and renamed class

--- modules/dheap/application/main.cpp
+++ modules/dheap/application/main.cpp
@@ -6,7 +6,7 @@
 #include "include/dheap_app.h"
 
 int main(int argc, const char* argv[]) {
-    DHeap::DHeapApp app;
+    DheapApp app;
     int retcode = 0;
     std::cout << app(argc, argv, &retcode);
     return retcode;
--- modules/dheap/include/dheap_app.h
+++ modules/dheap/include/dheap_app.h
@@ -4,19 +4,14 @@
 #define MODULES_DHEAP_INCLUDE_DHEAP_APP_H_
 
 #include <string>
-
-namespace DHeap {
-
-class DHeapApp {
+class DheapApp {
  public:
-    DHeapApp() = default;
+    DheapApp() = default;
     std::string operator()(int argc, const char* argv[],
                            int* retcode = nullptr) const;
 
  private:
     std::string help(const std::string& filename) const;
 };
-
-}  // namespace DHeap
 
 #endif  // MODULES_DHEAP_INCLUDE_DHEAP_APP_H_
